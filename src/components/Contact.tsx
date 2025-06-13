d8fb0e9939446bb0e690c0267c007847c4e8b973
Ledico <76535492+Ledico@users.noreply.github.com>
2025-06-13 23:12:41 +0200

    Updated Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -58,9 +58,9 @@ const Contact = () => {
   ];
 
   const socialLinks = [
-    { icon: Github, href: '#', label: 'GitHub' },
-    { icon: Linkedin, href: '#', label: 'LinkedIn' },
-    { icon: Twitter, href: '#', label: 'Twitter' }
+    { icon: Github, href: 'https://github.com/Ledico', label: 'GitHub' },
+    { icon: Linkedin, href: 'www.linkedin.com/in/leonardo-dias-costa', label: 'LinkedIn' },
+    { icon: GitLab, href: 'https://gitlab.com/leonardorafael.costa04', label: 'GitLab' },
   ];
 
   return (
