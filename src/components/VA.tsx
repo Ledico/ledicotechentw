29d6b291ed07c7470a3af9f23910553776ecc110
Ledico <76535492+Ledico@users.noreply.github.com>
2025-06-15 00:31:17 +0200

    Updated VA.tsx

--- src/components/VA.tsx
+++ src/components/VA.tsx
@@ -34,7 +34,7 @@ const VA = () => {
     if (isAutoPlaying) {
       slideInterval.current = setInterval(() => {
         nextSlide();
-      }, 5000);
+      }, 1000);
     } else {
       if (slideInterval.current) {
         clearInterval(slideInterval.current);
